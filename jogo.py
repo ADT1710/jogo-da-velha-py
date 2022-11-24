09e57ef4639a4069a1c7a88affe65c03f18f29c3
adt1710 <adrianoibira.adt@gmail.com>
2022-11-24 06:53:51 -0300

    Incluindo mais definições de Jogo

--- jogo.py
+++ jogo.py
@@ -19,3 +19,57 @@ class JogoDaVelha:
             if local == " ":
                 movimentos.append(1)
         return movimentos
+
+    def quadrados_vazios(self):
+        return " " in self.tabuleiro
+
+    def numeros_quadrados_vazios(self):
+        return self.tabuleiro.count(' ')
+
+    def faz_movimento(self,quadrado,letra):
+        if self.tabuleiro[quadrado] == ' ':
+            self.tabuleiro[quadrado] = letra
+            if self.vencedor(quadrado, letra):
+                self.vencedor_atual = letra
+            return True
+        return False
+
+    def vencedor(self,quadrado,letra):
+        linha_ind = quadrado // 3
+        linha = self.tabuleiro[linha_ind * 3: (linha_ind + 1) * 3]
+        if all([local == letra for local in linha]):
+            return True
+
+        coluna_ind = quadrado % 3
+        coluna = [self.tabuleiro[coluna_ind + i * 3] for i in range(3)]
+        if all([local == letra for local in coluna]):
+            return True
+
+
+def jogar(jogo, jogador_x, jogador_o, printa_jogo=True):
+    if printa_jogo:
+        jogo.printa_numeros_tabuleiro()
+
+    letra = 'X'
+    while jogo.quadrados_vazios():
+        if letra == 'O':
+            quadrado = jogador_o.pega_movimento(jogo)
+        else:
+            quadrado = jogador_x.pega_movimento(jogo)
+
+        if jogo.faz_movimento(quadrado,letra):
+            if printa_jogo:
+                print(f'{letra} fez um movimento para o quadrado {quadrado}')
+                jogo.printa_tabuleiro
+                print('')
+
+            if jogo.vencedor_atual:
+                if printa_jogo:
+                    print(f'{letra} venceu!')
+                return letra
+
+
+            letra = 'O' if letra == 'X' else 'X'
+
+        if printa_jogo:
+            print("É um Empate!")
